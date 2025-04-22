e852a204e92bda03b1950ced30416d64668b65ef
ndg23 <marvinndg242@gmail.com>
2025-04-22 09:28:05 +0000

    upgrade design home page

---
+++ components/ui/ContentContainer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div 
+    class="w-full max-w-7xl mx-auto px-6"
+    :class="{ 
+      'py-24': !noPadding,
+      [className]: className
+    }"
+  >
+    <slot />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  noPadding?: boolean
+  className?: string
+}>()
+</script> 
